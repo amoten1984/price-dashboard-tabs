b7f8a1e42337cf98d2a0f3ac6253b88ff0119785
amoten1984 <42321481+amoten1984@users.noreply.github.com>
2025-05-07 16:36:39 -0400

    Initial commit

---
+++ src/App.jsx
@@ -0,0 +1,9 @@
+import React from 'react';
+
+export default function App() {
+  return (
+    <div className='min-h-screen bg-gray-100 text-gray-900 p-4'>
+      <h1 className='text-2xl font-bold'>Tab Layout Coming Soon...</h1>
+    </div>
+  );
+}
