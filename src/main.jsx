8bd814a14fac5382350dbf793330642e0666c975
amoten1984 <42321481+amoten1984@users.noreply.github.com>
2025-05-12 17:36:18 -0400

    fix break

--- src/main.jsx
+++ src/main.jsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import './index.css';
 import ReactDOM from 'react-dom/client';
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import App from './App.jsx'; // This should be your landing page
